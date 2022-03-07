24fa5ccdec99aefa699e22c713013b390813940b
Bob Duffy <bob@prodata.ie>
2022-03-07 14:58:06 +0000

    Powershell Download

---
+++ IaaS/AzVmStats/Download-AzVmStats.ps1
@@ -0,0 +1,19 @@
+﻿$region = "northeurope"
+$Path= "C:\temp\AzComputeResourceSku.csv"
+(Get-AzComputeResourceSku -Location "northeurope"   ) | where {$_.ResourceType -eq "virtualMachines" } | Select-Object @{Name='vmName';   Expression={ $_.Name } ; } -ExpandProperty Capabilities | Export-Csv -Path $Path
+
+
+
+$Path= "C:\temp\AzCost.csv"
+if (Test-Path $Path) {
+  Remove-Item $Path
+}
+$Uri='https://prices.azure.com/api/retail/prices?currencyCode=''EUR''&$filter=serviceName eq ''Virtual Machines'' and priceType eq ''Consumption'' and armRegionName eq ''northeurope'''
+Do {
+    $Uri
+    $page=Invoke-RestMethod -Uri $Uri 
+    $Count=$page.Count
+    $Uri=$page.NextPageLink
+    $page.Items |  Select-Object CurrencyCode, armSkuName , productId, skuId, location, retailPrice, type ,  unitOfMeasure   | Export-Csv -Path $Path  -Append
+
+} while ($Uri)
